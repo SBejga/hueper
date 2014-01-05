49b717b298ba36017ecb70822f898d7594b1cba1
Michael Strobel <michael@kryops.de>
2014-01-05 15:56:28 +0100

    Arduino serial connection, Raspberry Pi button shutdown, Hue API error catching

---
+++ nodejs/server/controllers/arduino_button.js
@@ -0,0 +1,22 @@
+module.exports = function(app) {
+
+    /*
+     * Commodity function to properly shut down the Raspberry Pi
+     * with a button connected to the Arduino
+     */
+
+    // listening for {action: button} event
+    app.controllers.arduino.addListener(function(message) {
+        if(typeof(message.action) !== 'undefined' && message.action === 'button') {
+            console.log('[arduino_button] Button event received, shutting down');
+
+            try {
+                require('child_process').exec('sudo halt');
+            }
+            catch(e) {
+                console.log('[arduino_button] Shutdown error:', e);
+            }
+        }
+    });
+
+};
