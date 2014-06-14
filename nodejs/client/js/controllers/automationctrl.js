f67b9f4f9f07136fb3e1f04bea8ce7c8275c6429
Hanne Nobis <Hannenobis@gmx.net>
2014-06-14 20:32:50 +0200

    automation predefine values

--- nodejs/client/js/controllers/automationctrl.js
+++ nodejs/client/js/controllers/automationctrl.js
@@ -8,6 +8,7 @@ angular.module('hueApp.controllers').
         $scope.automation = {
             selectedAutomationId:0,
             selectedPropertyIndex:0,
+            saveChanges:false,
 
             selectedProperty:{
                 type:'',
@@ -44,9 +45,15 @@ angular.module('hueApp.controllers').
             },
 
             createTrigger: function(trigger){
-                $scope.state.automation[$scope.automation.selectedAutomationId].triggers.push(trigger);
-                socket.emit('automation.update', $scope.state.automation[$scope.automation.selectedAutomationId]);
-                $scope.automation.resetSelectedProperty();
+               if($scope.automation.testValueContent()){
+                   $scope.state.automation[$scope.automation.selectedAutomationId].triggers.push(trigger);
+                   socket.emit('automation.update', $scope.state.automation[$scope.automation.selectedAutomationId]);
+                   $scope.automation.resetSelectedProperty();
+                   $scope.sharedScope.submenu.closeSubmenu();
+               }
+               else{
+                   $scope.sharedScope.submenu.openSubmenu(['notificationValueIncomplete', 'trigger', 'createTrigger']);
+               }
             },
             createCondition: function(condition, conditionArray){
                 if(condition.type === 'weekdays'){
@@ -69,9 +76,15 @@ angular.module('hueApp.controllers').
             },
 
             updateTrigger: function(){
-                $scope.state.automation[$scope.automation.selectedAutomationId].triggers[$scope.automation.selectedPropertyIndex] = $scope.automation.selectedProperty;
-                socket.emit('automation.update', $scope.state.automation[$scope.automation.selectedAutomationId]);
-                $scope.automation.resetSelectedProperty();
+                if($scope.automation.testValueContent()){
+                    $scope.state.automation[$scope.automation.selectedAutomationId].triggers[$scope.automation.selectedPropertyIndex] = $scope.automation.selectedProperty;
+                    socket.emit('automation.update', $scope.state.automation[$scope.automation.selectedAutomationId]);
+                    $scope.automation.resetSelectedProperty();
+                    $scope.sharedScope.submenu.closeSubmenu();
+                }
+                else{
+                    $scope.sharedScope.submenu.openSubmenu(['notificationValueIncomplete', 'trigger', 'editTrigger']);
+                }
             },
             updateCondition: function(){
                 if($scope.automation.selectedProperty.type === 'weekdays'){
@@ -114,6 +127,14 @@ angular.module('hueApp.controllers').
 
             resetTriggerValue: function(t) {
                 t.value = undefined;
+                switch(t.type){
+                    case 'light':
+                        t.value = {'relation': '>'};
+                        break;
+                    case 'device':
+                        t.value = {'action': 'login'};
+                        break;
+                }
             },
             resetConditionValue: function(t) {
                 if(t.type === 'state') {
@@ -142,12 +163,10 @@ angular.module('hueApp.controllers').
                 $scope.state.automation[id].active = !$scope.state.automation[id].active;
                 $scope.automation.update($scope.state.automation[id]);
             },
-
             toggleSingle: function(id){
                 $scope.state.automation[id].single = !$scope.state.automation[id].single;
                 $scope.automation.update($scope.state.automation[id]);
             },
-
             toggleAllConditionsNeeded: function(id){
                 $scope.state.automation[id].allConditionsNeeded = !$scope.state.automation[id].allConditionsNeeded;
                 $scope.automation.update($scope.state.automation[id]);
@@ -164,14 +183,28 @@ angular.module('hueApp.controllers').
                 console.log($scope.automation.selectedProperty);
             },
 
-            resetSelectedProperty: function(){
-                $scope.automation.selectedProperty = {
-                    type:'',
-                    value: undefined
-                };
-                $scope.automation.selectedPropertyIsArray = {
-                    array: []
-                };
+            resetSelectedProperty: function(initiator){
+                if((initiator === 'trigger') || (initiator === 'condition') ){
+                    $scope.automation.selectedProperty = {
+                        type:'light',
+                        value: {
+                            relation:'>',
+                            threshold: undefined
+                        }
+                    };
+                    $scope.automation.selectedPropertyIsArray = {
+                        array: []
+                    };
+                }else if(initiator === 'action'){
+                    $scope.automation.selectedProperty = {
+                        type:'light',
+                        value: undefined
+                    };
+                    $scope.automation.selectedPropertyIsArray = {
+                        array: []
+                    };
+                }
+
             },
 
             setSelectedAutomationId: function(id){
@@ -183,7 +216,176 @@ angular.module('hueApp.controllers').
                 $scope.automation.setSelectedAutomationId($scope.helpers.urlId);
                 $scope.automation.setSelectedPropertyIndex(index);
                 $scope.automation.setSelectedProperty({'type': type, 'value' : value});
+            },
+
+            testValueContentForTrigger: function(){
+                var p = $scope.automation.selectedProperty;
+                switch(p.type){
+                    case 'light':
+                        if(p.value === undefined){
+                            return false;
+                        }else if((p.value.relation != undefined) && (p.value.threshold != undefined)){
+                            if(p.value.threshold < 0){
+                                p.value.threshold = 0;
+                            }else if(p.value.threshold > 100){
+                                p.value.threshold = 100;
+                            }
+                            return true;
+                        }else{
+                            return false;
+                        }
+                        break;
+                    case 'motion':
+                        return true;
+                        break;
+
+                    case 'speech':
+                        if(p.value === undefined){
+                            return false;
+                        }else if(p.value.length > 0){
+                            return true;
+                        }else{
+                            return false;
+                        }
+                        break;
+
+                    case 'schedule':
+                        if(p.value === undefined){
+                            return false;
+                        }else if((p.value.hour != undefined) && (p.value.minute != undefined)){
+                            if((p.value.hour < 0) || (p.value.hour > 23)){
+                                p.value.hour = 8;
+                            }
+                            if((p.value.minute < 0) || (p.value.minute > 59)){
+                                p.value.minute = 0;
+                            }
+                            return true;
+                        }else{
+                            return false;
+                        }
+                        break;
+
+                    case 'periodical':
+                        if(p.value === undefined){
+                            return false;
+                        }else{
+                            return true;
+                        }
+                        break;
+
+                    case 'rfid':
+                        if(p.value === undefined){
+                            return false;
+                        }else{
+                            return true;
+                        }
+                        break;
+
+                    case 'device':
+                        if((p.value.action != undefined) && (p.value.address != undefined)){
+                            return true;
+                        }else{
+                            return false;
+                        }
+                        break;
+
+                    case 'custom':
+                        if(p.value === undefined){
+                            return false;
+                        }else if(p.value.length > 0){
+                            return true;
+                        }else{
+                            return false;
+                        }
+                        break;
+                }
+            },
+
+            testValueContent: function(){
+                var p = $scope.automation.selectedProperty;
+                switch(p.type){
+                    case 'light':
+                        if(p.value === undefined){
+                            return false;
+                        }else if((p.value.relation != undefined) && (p.value.threshold != undefined)){
+                            if(p.value.threshold < 0){
+                                p.value.threshold = 0;
+                            }else if(p.value.threshold > 100){
+                                p.value.threshold = 100;
+                            }
+                            return true;
+                        }else{
+                            return false;
+                        }
+                        break;
+                    case 'motion':
+                        return true;
+                        break;
+
+                    case 'speech':
+                        if(p.value === undefined){
+                            return false;
+                        }else if(p.value.length > 0){
+                            return true;
+                        }else{
+                            return false;
+                        }
+                        break;
+
+                    case 'schedule':
+                        if(p.value === undefined){
+                            return false;
+                        }else if((p.value.hour != undefined) && (p.value.minute != undefined)){
+                            if((p.value.hour < 0) || (p.value.hour > 23)){
+                                p.value.hour = 8;
+                            }
+                            if((p.value.minute < 0) || (p.value.minute > 59)){
+                                p.value.minute = 0;
+                            }
+                            return true;
+                        }else{
+                            return false;
+                        }
+                        break;
+
+                    case 'periodical':
+                        if(p.value === undefined){
+                            return false;
+                        }else{
+                            return true;
+                        }
+                        break;
+
+                    case 'rfid':
+                        if(p.value === undefined){
+                            return false;
+                        }else{
+                            return true;
+                        }
+                        break;
+
+                    case 'device':
+                        if((p.value.action != undefined) && (p.value.address != undefined)){
+                            return true;
+                        }else{
+                            return false;
+                        }
+                        break;
+
+                    case 'custom':
+                        if(p.value === undefined){
+                            return false;
+                        }else if(p.value.length > 0){
+                            return true;
+                        }else{
+                            return false;
+                        }
+                        break;
+                }
             }
+
+
+
         };
 
 
